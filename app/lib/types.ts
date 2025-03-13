f804acefee042bc2d982bf3b4495ac1284401bdd
Elda Moudy <elda@moudy.net>
2025-03-13 19:21:21 -0400

    updating types

--- app/lib/types.ts
+++ app/lib/types.ts
@@ -1,35 +1,34 @@
 export type UserInterface  = {
-	userId: string;
+	id: number;
 	name: string;
 	email: string;
 };
 
 export type ChatSessionInterface  = {
-	chatSessionId: string;
-	chatSessionName: string;
-	userId: string;
-	createdAt: string; // ISO 8601 date-time string
-	updatedAt: string; // ISO 8601 date-time string
+	id: number;
+	name: string;
+	userId: number;
+	createdAt: Date; // YYYY-MM-DD format
+	updatedAt: Date; // YYYY-MM-DD format
 };
 
 export type MessageInterface  = {
-  messageId: string;
-  chatSessionId: string;
-  userInput: string;
-  aiInput: string;
-  messageTimestamp: string; // ISO 8601 date-time string
+  id: number;
+  chatSessionId: number;
+  userQuery: string;
+  aiResponse: string;
+  messageTimestamp: Date; // ISO 8601 date-time string
 };
 
 export type RecipeInterface = {
-  recipeId: string;
-  userId: string;
+  id: number;
   name: string;
   category: string;
   description: string;
   servings: number;
   prepTime: number; // In minutes
   cookTime: number; // In minutes
-  createdAt: string; // YYYY-MM-DD format
-  updatedAt: string; // YYYY-MM-DD format
-  lastViewed: string; // YYYY-MM-DD format
+  createdAt: Date; // YYYY-MM-DD format
+  updatedAt: Date; // YYYY-MM-DD format
+  lastViewed: Date; // YYYY-MM-DD format
 };
