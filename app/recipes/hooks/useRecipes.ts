30771178dadac92acc5f4bcdbd14b1900e5a6351
Elda Moudy <elda@moudy.net>
2025-03-15 22:15:11 -0400

    using RecipeService

--- app/recipes/hooks/useRecipes.ts
+++ app/recipes/hooks/useRecipes.ts
@@ -1,29 +1,17 @@
 "use client";
 import useSWR from "swr";
 import { useSearchParams } from "next/navigation";
-import { recipes } from "@/lib/placeholder-data";
+import RecipeService from "@/recipes/lib/recipeService";
 
 
 export function useRecipes() {
-  const fetcher = (url: string) => 
-    fetch(url).then(res => res.json())
-      .then(data => 
-        data.map((recipe: any) => ({
-          ...recipe,
-          createdAt: new Date(recipe.created_at),
-          updatedAt: new Date(recipe.updated_at),
-          lastViewed: new Date(recipe.last_viewed),
-        }))
-      );
-
   const searchParams = useSearchParams();
-
   const category = searchParams.get("category") || "";
   const page = parseInt(searchParams.get("page") || "1", 10);
 
   const { data, error, isLoading, mutate } = useSWR(
     `/api/recipes?category=${encodeURIComponent(category)}&page=${page}`,
-    fetcher
+    () => RecipeService.fetchRecipes()
   );
 
   return {
