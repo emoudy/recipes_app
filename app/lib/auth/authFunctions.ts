d5c92809f9881337d5276b7f2e8e840197b9b79b
Elda Moudy <elda@moudy.net>
2025-03-30 07:27:32 -0400

    removing hardcoded return in auth

--- app/lib/auth/authFunctions.ts
+++ app/lib/auth/authFunctions.ts
@@ -9,33 +9,22 @@ declare module "next-auth" {
   }
 }
 
-/** 🔹 Hashes a user's password */
+/** Hashes a user's password */
 // export async function hashPassword(password: string): Promise<string> {
 //   const saltRounds = 10;
 //   return bcrypt.hash(password, saltRounds);
 // }
 
-/** 🔹 Verifies a user's password */
 export async function verifyPassword(password: string, hashedPassword: string): Promise<boolean> {
   return bcrypt.compare(password, hashedPassword);
 }
 
 export async function getValidatedSession(): Promise<Session> {
-  // const session: Session | null = await auth();
-  // console.log("getValidatedSession session", session)
+  const session: Session | null = await auth();
 
-  // if (!session || !session.user?.id) {
-  //   console.log("Not Working")
-  //   throw new Error("Unauthorized - Missing user ID"); 
-  // }
-
-  const session = {
-    user: {
-      id: 1,
-      name: "Alice",
-      email: "alice@example.com"
-    },
-    expires: new Date().toISOString()
+  if (!session || !session.user?.id) {
+    console.log("Not Working")
+    throw new Error("Unauthorized - Missing user ID"); 
   }
 
   return session;
